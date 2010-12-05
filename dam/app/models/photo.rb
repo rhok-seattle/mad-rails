bce552857fd31825992af69469ca6da2dc88bce1
Zach Hale <zachhale@gmail.com>
2010-12-04 16:15:09 -0800

    improved xml out put of photo objects

--- dam/app/models/photo.rb
+++ dam/app/models/photo.rb
@@ -2,4 +2,17 @@ class Photo < ActiveRecord::Base
   belongs_to :residence
 
   has_attached_file :image, :styles => {:thumb => "100x100>"}
+
+  alias :ar_to_xml :to_xml
+  def to_xml(options = {})
+    procs = [
+      Proc.new { |options| options[:builder].tag!('image-path', image.url) },
+      Proc.new { |options| options[:builder].tag!('thumbnail-image-path', image.url(:thumb)) }
+    ]
+
+    options[:except] ||= []
+    options[:except] += [:image_file_name]
+
+    ar_to_xml(options.merge(:procs => procs))
+  end
 end
